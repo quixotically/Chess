c7f61cc61819bc86e5ceab9dea548918dae027c2
felicia <quixoticf@gmail.com>
2015-06-29 22:44:51 -0400

    Refactor turn status in Game

--- lib/game.rb
+++ lib/game.rb
@@ -4,9 +4,11 @@ require_relative 'board'
 require_relative 'human_player'
 
 class Game
-  attr_accessor :board, :player1, :player2
+  attr_accessor :board, :player1, :player2, :turn_num
 
   def initialize(board = nil, num_players)
+    @turn_num = 1
+
     if board.nil?
       @board = Board.populated_board
     else
@@ -25,20 +27,11 @@ class Game
   def play
     self.set_player_colors
     active_player = (player1.color == :white ? player1 : player2)
-    turn_no = 1
 
     puts "White goes first."
 
     until board.checkmate?(:white) || board.checkmate?(:black)
-      # play_turn turn_no ||= 1
-      # turn_msg
-      puts "Turn number: #{turn_no}"
-      puts "Active player: #{active_player.color.capitalize}"
-      if board.in_check?(active_player.color)
-        puts "#{active_player.color.capitalize} in check!"
-      end
-      # turn_msg end
-
+      self.turn_status(active_player)
       board.display_grid
 
       begin
@@ -56,17 +49,28 @@ class Game
       end
 
       active_player = (active_player == player1 ? player2 : player1)
-      turn_no += 1
+      @turn_num += 1
     end
 
     winner = (active_player == player1 ? player2 : player1)
     puts "#{winner.color.capitalize} wins!"
-    # play_turn end
+  end
+
+  def play_turn
+
   end
 
   def set_player_colors
     player1.select_color
     player1.color == :white ? player2.set_color(:black) : player2.set_color(:white)
+  end
+
+  def turn_status(active_player)
+    puts "Turn number: #{turn_num}"
+    puts "Active player: #{active_player.color.capitalize}"
+    if board.in_check?(active_player.color)
+      puts "#{active_player.color.capitalize} in check!"
+    end
   end
 end
 
